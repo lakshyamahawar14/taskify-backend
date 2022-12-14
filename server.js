3837ae8618446e3acfa25a19e242209b69c9812c
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2022-12-14 18:48:42 +0530

    setting up routes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
-
 const app = express()
+const tasks = require('./routes/tasks');
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Task Manager App');
+    res.send('server is running');
 })
+
+app.use('/api/v1/tasks', tasks)
 
 const PORT = 5000
 
